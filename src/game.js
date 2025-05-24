53d2afd790728b9b1a2c06bf8be4c683350e66a3
charles-254 <kamaucharles6884@gmailcom>
2025-05-24 09:31:02 +0300

    Implement computer random choice and responsive design
    - Built logic for computer to randomly choose rock, paper, or scissors
    - Displayed computer's random choice in the user interface
    - Added media queries to make the page responsive across different
      screen sizes

--- src/game.js
+++ src/game.js
@@ -2,75 +2,65 @@ let computerScoreValue = 0;
 let playerScoreValue = 0;
 let computerScoreElement = document.querySelector(".computer-score");
 let playerScoreElement = document.querySelector(".player-score");
-let playerChoiceElement = document.querySelector(".player-choice")
-let computerChoiceElement = document.querySelector(".computer-choice")
+let playerChoiceElement = document.querySelector(".player-choice");
+let computerChoiceElement = document.querySelector(".computer-choice");
 
-const scissorsElement = document.querySelector(".scissors")
-const rockElement = document.querySelector(".rock")
-const paperElement = document.querySelector(".paper")
+const scissorsElement = document.querySelector(".scissors");
+const rockElement = document.querySelector(".rock");
+const paperElement = document.querySelector(".paper");
 
 const scissors = "✌️";
 const paper = "✋";
 const rock = "✊";
 
-let computerChoice = rock
-let playerChoice = ""
+let computerChoice = "";
+let playerChoice = "";
 
-function checkWinner(){
-  let showResults = document.querySelector(".show-results-text")
-  if (playerChoice === computerChoice){
-    showResults.textContent = `DRAW`
-
-  }else if (playerChoice === scissors && computerChoice === paper){
-    showResults.textContent = `PLAYER WINS`
-    playerScoreValue += 1
-    playerScoreElement.textContent = playerScoreValue
-
-  }else if (playerChoice === scissors && computerChoice === rock){
-    showResults.textContent = `COMPUTER WINS`
-    computerScoreValue += 1
-    computerScoreElement.textContent = computerScoreValue
-    
-  }else if (playerChoice === paper && computerChoice === rock){
-    showResults.textContent = `PLAYER WINS`
-    playerScoreValue += 1
-    playerScoreElement.textContent = playerScoreValue
-
-  }else if (playerChoice === paper && computerChoice === scissors){
-    showResults.textContent = `COMPUTER WINS`
-    computerScoreValue += 1
-    computerScoreElement.textContent = computerScoreValue
-
-  }else if (playerChoice === rock && computerChoice === paper){
-    showResults.textContent = `COMPUTER WINS`
-    computerScoreValue += 1
-    computerScoreElement.textContent = computerScoreValue
-
-  }else if (playerChoice === rock && computerChoice === scissors){
-    showResults.textContent = `PLAYER WINS`
-    playerScoreValue += 1
-    playerScoreElement.textContent = playerScoreValue
-
-  }else {
-    console.log("Invalid game options")
+function checkWinner() {
+  let showResults = document.querySelector(".show-results-text");
+  if (playerChoice === computerChoice) {
+    showResults.textContent = `DRAW`;
+  } else if (
+    (playerChoice === scissors && computerChoice === paper) ||
+    (playerChoice === paper && computerChoice === rock) ||
+    (playerChoice === rock && computerChoice === scissors)
+  ) {
+    showResults.textContent = `PLAYER WINS`;
+    playerScoreValue += 1;
+    playerScoreElement.textContent = playerScoreValue;
+  } else if (
+    (playerChoice === scissors && computerChoice === rock) ||
+    (playerChoice === paper && computerChoice === scissors) ||
+    (playerChoice === rock && computerChoice === paper)
+  ) {
+    showResults.textContent = `COMPUTER WINS`;
+    computerScoreValue += 1;
+    computerScoreElement.textContent = computerScoreValue;
+  } else {
+    console.log("Invalid game options");
   }
-    
 }
 
+function computerRandomChoice() {
+  let choices = ["✊", "✋", "✌️"];
+  let choice = choices[Math.floor(Math.random() * choices.length)];
+  return choice;
+}
 
-scissorsElement.addEventListener("click", function (){
-  playerChoice = scissors
-  playerChoiceElement.textContent = scissors
-  checkWinner()
-})
-rockElement.addEventListener("click", function (){
-  playerChoice = rock
-  playerChoiceElement.textContent = rock
-  checkWinner()
-})
-paperElement.addEventListener("click", function (){
-  playerChoice = paper
-  playerChoiceElement.textContent = paper
-  checkWinner()
-})
+function setChoices(choice) {
+  playerChoice = choice;
+  playerChoiceElement.textContent = playerChoice;
+  computerChoice = computerRandomChoice();
+  computerChoiceElement.textContent = computerChoice;
+  checkWinner();
+}
 
+scissorsElement.addEventListener("click", function () {
+  setChoices(scissors);
+});
+rockElement.addEventListener("click", function () {
+  setChoices(rock);
+});
+paperElement.addEventListener("click", function () {
+  setChoices(paper);
+});
